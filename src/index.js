d7bd66bc0edf265b0e14ba5ff068bb8b817278a0
Vivek Sharma <viveksdf@gmail.com>
2026-05-06 09:47:05 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -27,7 +27,19 @@ const app = express();
 // ============================================================================
 // MIDDLEWARE
 // ============================================================================
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:3000",
+    "http://13.232.201.136:3000",
+    "https://13.232.201.136:3000",
+    "http://13.232.201.136:5173",
+    "https://13.232.201.136:5173",
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 app.use(express.json());
 app.use(requestLogger);
 
